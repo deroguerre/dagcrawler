e72db2b7b90944cc5381b899920d7b9abf8b1f86
gui <deroguerre@gmail.com>
2021-10-01 14:10:19 +0200

    add more log informations

--- extract.js
+++ extract.js
@@ -15,10 +15,13 @@ var maxtimeout = 0
 
 
 
-
+var startTime, endTime;
 const ipfs = await IPFS.create()
-
 const validCID = CID.parse(stringCID)
+const foundData = []
+const today = new Date()
+var totalObj = 0
+var nbFound = 0
 
 // Clear crawled_data before crawling
 try {
@@ -27,56 +30,66 @@ try {
     console.log("no file")
 }
 
+startTimer()
+
 //make a ls on dag cid
 for await (const file of ipfs.ls(validCID, { 'timeout': maxtimeout })) {
     // console.log(file)
-    getDataFromObject(file);
+    getDataFromObject(file)
 }
+
+endTimer()
+console.log("searching DONE " + nbFound + " found on " + totalObj)
+process.exit()
 
 async function getDataFromObject(file) {
 
-    let today = new Date();
+    totalObj++
     let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getUTCSeconds()
-    console.log(time + " searching in " + file.path + " size " + file.size + "... ctrl + c to abort")
+    console.log(nbFound + "/" + totalObj + " " + time + " searching in " + file.path + " size " + file.size + "... ctrl + c to abort")
 
     let data = await ipfs.object.data(file.cid)
     let stringData = data.toString()
-    
+
     if (stringData.match(new RegExp(searchedWord, "i"))) {
 
-        console.log(file.cid + " found")
+        nbFound++
+        console.log('\x1b[32m%s\x1b[0m', nbFound + "/" + totalObj + " " + time + " searching in " + file.path + " size " + file.size + " => " + file.cid + " found")
 
         stringData = removeNonAscii(stringData)
 
-        let objData = JSON.parse(stringData);
+        let objData = JSON.parse(stringData)
         objData.cid = file.cid.toString()
         objData.path = file.path
+        // foundData.push(objData)
 
+        // Save file
         fs.appendFileSync('crawled_data.json', JSON.stringify(objData))
+
     }
 }
 
 function removeNonAscii(str) {
 
     if ((str === null) || (str === ''))
-        return false;
+        return false
     else
-        str = str.toString();
+        str = str.toString()
 
-    return str.replace(/[^\x20-\x7E]/g, '');
+    return str.replace(/[^\x20-\x7E]/g, '')
 }
 
-console.log("searching DONE !")
+function startTimer() {
+    startTime = new Date()
+}
 
-// const testCidObject = CID.parse('QmeqNQdjbKjrG9q19L33u2J7su8ksbfmLCmAHB5daXraJH')
+function endTimer() {
+    endTime = new Date()
+    var timeDiff = endTime - startTime //in ms
+    // strip the ms
+    timeDiff /= 1000
 
-// const data = await ipfs.object.data(testCidObject)
-// console.log(data.toString())
-
-// async function getAndLog(cid, path) {
-//     const result = await ipfs.dag.get(cid, { path })
-//     console.log(result.value)
-//     return result.value;
-// }
-
-// const loggedCid = getAndLog(fromString, '/10')
+    // get seconds 
+    var seconds = Math.round(timeDiff)
+    console.log(seconds + " seconds")
+}
